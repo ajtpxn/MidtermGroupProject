f2784399384ef251875db035b62d161719f12b0e
Henry Zelenka <hcz2@georgetown.edu>
2018-10-25 15:46:17 -0600

    Make basic versions of TransactionDAO and TransactionDAOImpl

---
+++ Book2Book/src/main/java/com/skilldistillery/book2book/data/TransactionDAOImpl.java
@@ -0,0 +1,33 @@
+package com.skilldistillery.book2book.data;
+
+import java.util.List;
+
+import com.skilldistillery.book2book.entities.Transaction;
+
+public class TransactionDAOImpl implements TransactionDAO {
+
+	@Override
+	public Transaction getTransactionById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Transaction> getTransactionsByBorrowerId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Transaction> getTransactionsByLenderId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Transaction> getTransactionsByCopyId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
