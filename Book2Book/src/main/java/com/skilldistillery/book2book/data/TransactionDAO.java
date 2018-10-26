fb76993706fe31a927f8c88dcf793bbd0e050cde
Henry Zelenka <hcz2@georgetown.edu>
2018-10-26 08:53:00 -0600

    Flesh out TransactionDAO and TransactionDAOImpl some more

--- Book2Book/src/main/java/com/skilldistillery/book2book/data/TransactionDAO.java
+++ Book2Book/src/main/java/com/skilldistillery/book2book/data/TransactionDAO.java
@@ -5,8 +5,9 @@ import java.util.List;
 import com.skilldistillery.book2book.entities.Transaction;
 
 public interface TransactionDAO {
+	public Transaction makeTransaction(Transaction t);
 	public Transaction getTransactionById(int id);
-	public List<Transaction> getTransactionsByBorrowerId(int id);
-	public List<Transaction> getTransactionsByLenderId(int id);
-	public List<Transaction> getTransactionsByCopyId(int id);
+	public List<Object[]> getTransactionsByBorrowerId(int id);
+	public List<Object[]> getTransactionsByLenderId(int id);
+	public List<Object[]> getTransactionsByCopyId(int id);
 }
