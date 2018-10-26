13ea0fc51d5dbf9ae3bf53e5d234fa45f4a2b9db
Henry Zelenka <hcz2@georgetown.edu>
2018-10-26 09:49:24 -0600

    Expand TransactionDAO and TransactionDAOImpl

--- Book2Book/src/main/java/com/skilldistillery/book2book/data/TransactionDAO.java
+++ Book2Book/src/main/java/com/skilldistillery/book2book/data/TransactionDAO.java
@@ -6,6 +6,7 @@ import com.skilldistillery.book2book.entities.Transaction;
 
 public interface TransactionDAO {
 	public Transaction makeTransaction(Transaction t);
+	public Transaction updateTransaction(int id, Transaction updated);
 	public Transaction getTransactionById(int id);
 	public List<Object[]> getTransactionsByBorrowerId(int id);
 	public List<Object[]> getTransactionsByLenderId(int id);
