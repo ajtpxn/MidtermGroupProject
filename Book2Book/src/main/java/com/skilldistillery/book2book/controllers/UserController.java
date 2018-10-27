645753ba6482035108836634d66689137c1d9177
ajtpxn <ajtpxn@gmail.com>
2018-10-27 09:53:59 -0600

    Login partially working

--- Book2Book/src/main/java/com/skilldistillery/book2book/controllers/UserController.java
+++ Book2Book/src/main/java/com/skilldistillery/book2book/controllers/UserController.java
@@ -23,12 +23,14 @@ public class UserController {
 	
 	@RequestMapping(path="index.do")
 	public ModelAndView index() {
+		System.out.println("index");
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("index.jsp");
 		return mv;
 	}
 	
 	public void addUser(User user) {
+		System.out.println("addUser");
 		userDAO = new UserDAOimpl();
 		ModelAndView mv = new ModelAndView();
 		userDAO.creatUser(user);
@@ -39,16 +41,19 @@ public class UserController {
 	@RequestMapping(path="status.do")
 	@ModelAttribute("userStatus")
 	public boolean userIsLoggedIn(HttpSession session) {
+		System.out.println("userIsLoggedIn");
 		boolean status = false;
 		User user = (User) session.getAttribute("USER");
 		if (user != null) {
 			status = true;
 		}
+		System.out.println(status);
 		return status;
 	}
 	
 	@RequestMapping(path="login.do", method=RequestMethod.GET)
 	public ModelAndView loginFromHome(HttpSession session) {
+		System.out.println("loginFromHome");
 	    ModelAndView mv = new ModelAndView();
 	    
 	    if (userIsLoggedIn(session)) {
@@ -65,48 +70,29 @@ public class UserController {
 	}
 	
 	@RequestMapping(path="login.do", method=RequestMethod.POST)
-	public ModelAndView logIn() {
+	public ModelAndView logIn(HttpSession session) {
+		System.out.println("logIn");
 		ModelAndView mv = new ModelAndView();
-		
 		userDAO = new UserDAOimpl();
-		
-		
-		/*if(userIsLoggedIn(session)) {
-			mv.setViewName("index.do");
+		if(userIsLoggedIn(session)) {
+			System.out.println("User is logged in******");
+			mv.setViewName("fail.jsp");
 			return mv;
 		}
-		User inDAO = userDAO.getUserByCredentials("usermcuserface", "imauser");
-		
-		if (inDAO != null) { // successful login
-			session.setAttribute("USER", inDAO);
-			mv.setViewName("account.do");
-		}
-		else {  // fail to login
-			mv.addObject("fail", true);
-			mv.setViewName("login.do");
-		}
-		
-		User user = new User();
-		mv.addObject("user", user);*/
-		
-		
-		
-		
-		
 		User validUser = userDAO.getUserByCredentials("usermcuserface", "imauser");
 		
 		if (validUser != null) {
+			session.setAttribute("USER", validUser);
 			mv.setViewName("success.jsp");
 		}
 		else {
 			mv.setViewName("fail.jsp");
 		}
-		
 		userDAO = null;
 		return mv;
 	}
-	
 	public void logOut() {
+		System.out.println("logOut");
 		
 	}
 
