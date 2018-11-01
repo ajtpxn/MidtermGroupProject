109dd974dcb56cb64578a82b3559cb14d3c0d2bb
Andy Iwamoto <aiwamoto789@gmail.com>
2018-11-01 14:22:29 -0600

    added error handling to add book field

--- Book2Book/src/main/java/com/skilldistillery/book2book/controllers/BookController.java
+++ Book2Book/src/main/java/com/skilldistillery/book2book/controllers/BookController.java
@@ -31,34 +31,83 @@ public class BookController {
 	private TransactionDAO td;
 	@Autowired
 	private UserDAO ud;
-
-	// ADD NEW BOOK TO DB
-	@RequestMapping(path = "add.do", method = RequestMethod.POST)
-
-	public String addNewBook(@RequestParam("genreName") int[] genreid, @RequestParam("contentRating.id") int contentId,
-			Book book) {
-
-		List<Genre> genres = new ArrayList<>();
-		for (int i : genreid) {
-			Genre genre = bd.findGenreById(i);
-			genres.add(genre);
-		}
-		ContentRating rating = bd.findContentRatingById(contentId);
-
-		book.setContentRating(rating);
-		book.setGenres(genres);
-
-		bd.addBook(book);
-
-		return "redirect:addbook.do";
-	}
-
-	@RequestMapping(path = "addbook.do", method = RequestMethod.GET)
-	public ModelAndView addedBook() {
+	
+//	// ADD NEW BOOK TO DB
+//	@RequestMapping(path = "add.do", method = RequestMethod.POST)
+//	public String addNewBook(@RequestParam("genreName") int[] genreid, @RequestParam("contentRating.id") int contentId,
+//			Book book) {
+//
+//		List<Genre> genres = new ArrayList<>();
+//		for (int i : genreid) {
+//			Genre genre = bd.findGenreById(i);
+//			genres.add(genre);
+//		}
+//		ContentRating rating = bd.findContentRatingById(contentId);
+//
+//		book.setContentRating(rating);
+//		book.setGenres(genres);
+//
+//		bd.addBook(book);
+//
+//		return "redirect:addbook.do";
+//	}
+//	
+//	
+//	@RequestMapping(path = "addbook.do", method = RequestMethod.GET)
+//	public ModelAndView addedBook() {
+//		ModelAndView mv = new ModelAndView();
+//		
+//		mv.setViewName("book");
+//
+//		return mv;
+//	}
+	
+	@RequestMapping(path = "addbook.do")
+	public ModelAndView addBook() {
+		System.out.println("Add Book");
 		ModelAndView mv = new ModelAndView();
-
-		mv.setViewName("book");
-
+		Book book = new Book();
+		mv.addObject(book);
+		mv.setViewName("add");
+		return mv;
+	}
+	
+	@RequestMapping(path = "add.do", method = RequestMethod.POST)
+	public ModelAndView returnBook(Book book) {
+		System.out.println("Return Book");
+		// userDAO = new UserDAOimpl();
+		ModelAndView mv = new ModelAndView();
+		List<Book> allBooks = bd.listAllBooks();
+		List<String> allTitles = new ArrayList<>();
+		for (Book thisBook : allBooks) {
+			allTitles.add(thisBook.getTitle());
+		}
+		boolean takenTitle = false;
+		for (String thisTitle : allTitles) {
+			if (thisTitle.equals(book.getTitle())) {
+				takenTitle = true;
+			}
+		}
+		System.out.println(book.getTitle());
+		
+		if (book.getTitle() == null || book.getTitle().equals("") || takenTitle) {
+			mv.setViewName("fail");
+			mv.addObject("titleFail", true);
+			return mv;
+		}
+		if (book.getAuthor() == null || book.getAuthor().equals("")) {
+			mv.setViewName("fail");
+			return mv;
+		}
+		if (book.getDescription() == null || book.getDescription().equals("")) {
+			mv.setViewName("fail");
+			return mv;
+		}
+		bd.addBook(book);
+		// bookDAO = null;
+		mv.addObject(book);
+		
+		mv.setViewName("success");
 		return mv;
 	}
 
